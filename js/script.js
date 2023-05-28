7cb9f03947edc8e7cfaa847cd67d64a99fa6a04b
unknown <you@example.com>
2023-05-28 18:11:45 -0500

    primer commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+function metodo() {
+    const cd = document.getElementById('lista');
+    const ads = document.getElementById('J');
+
+    cd.addEventListener('click',() =>{
+        alert(123654789)
+    });
+}
+
+
+
+window.addEventListener("load",metodo)
